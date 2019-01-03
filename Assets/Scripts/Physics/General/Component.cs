8551443b7fac2c4ef435f0ad0f8acdf8859e8b89
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-01-03 01:15:56 +0100

    Added numbering for components (shared between all components)

--- Assets/Scripts/Physics/General/Component.cs
+++ Assets/Scripts/Physics/General/Component.cs
@@ -2,6 +2,22 @@ namespace LoonyEngine {
 
     public abstract class Component {
 
+        #region [Static]
+
+        // TODO; theoretically this number could exceed the limits
+        // It also is shared by all components and not component specific -.-
+        // I would definitely do that different, with components beeing located in differeent list per component and then refilling gaps
+        // For now, I at least could provide a dictionary
+        static uint s_idCount = 0;
+
+        #endregion
+
+        #region [FinalVariables]
+
+        protected uint f_id;
+
+        #endregion
+
         #region [PrivateVariables]
 
         protected GameObject m_gameObject;
@@ -10,6 +26,7 @@ namespace LoonyEngine {
 
         #region [Properties]
 
+        public uint ID { get { return f_id; } }
         public GameObject GameObject { get { return m_gameObject; } }
 
         #endregion
@@ -18,6 +35,9 @@ namespace LoonyEngine {
 
         public Component(GameObject gameObject) {
             m_gameObject = gameObject;
+
+            f_id = s_idCount;
+            ++s_idCount;
         }
 
         #endregion
