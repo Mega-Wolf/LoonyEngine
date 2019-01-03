874cea7fb11a37e3ab3afe41b96ca9fb416572b0
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-01-03 11:26:19 +0100

    Rigidbody.UpdateDynamics (although ugly)

--- Assets/Scripts/Physics/General/Rigidbody.cs
+++ Assets/Scripts/Physics/General/Rigidbody.cs
@@ -6,16 +6,21 @@ namespace LoonyEngine {
         public ObjectData ObjectData { get; set; }
         public ColliderData ColliderData { get; set; }
 
-        public Rigidbody(GameObject gameObject) : base(gameObject) {
-
-        }
+        public Rigidbody(GameObject gameObject) : base(gameObject) {}
 
         public void UpdateDynamics() {
-            //TODO
+            //TODO; this has a lot ov overhead and will be changed later on
+            DynamicData dd = DynamicData;
+            dd.Velocity += DynamicData.Acceleration * SuperPhysicsManager.Instance.DELTA_TIME;
+            DynamicData = dd;
+
+            // TODO; it might not be possible to do that in the future (calling transform directly)
+            GameObject.Transform.Position += DynamicData.Velocity * SuperPhysicsManager.Instance.DELTA_TIME;
         }
 
         public void UpdateAABB() {
             //TODO
+            // That is dependend on the collider data
         }
 
     }
