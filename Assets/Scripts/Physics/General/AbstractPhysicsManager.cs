66c6a160bad6967ac24888d27691696d2cb2989e
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-01-07 07:17:22 +0100

    AbstractPM remembers if triggers/collisions are new or old

--- Assets/Scripts/Physics/General/AbstractPhysicsManager.cs
+++ Assets/Scripts/Physics/General/AbstractPhysicsManager.cs
@@ -29,12 +29,17 @@ namespace LoonyEngine {
 
         #endregion
 
-        #region [PrivateVariables]
+        #region [FinalVariables]
 
-        private HashSet<ulong> f_collisionsLastRound = new HashSet<ulong>();
-        private HashSet<ulong> f_collisionsThisRound = new HashSet<ulong>();
+        protected PhysicsMatrix f_physicsMatrix;
 
         private Dictionary<ulong, CheckState> f_checkStates = new Dictionary<ulong, CheckState>();
+
+        protected List<ObjectOrderInformation> f_oois = new List<ObjectOrderInformation>();
+
+        #endregion
+
+        #region [PrivateVariables]
 
         protected int m_moved = 0;
         private int m_broadChecks = 0;
@@ -42,9 +47,11 @@ namespace LoonyEngine {
         private int m_triggers = 0;
         private int m_collisions = 0;
 
-        protected List<ObjectOrderInformation> f_oois = new List<ObjectOrderInformation>();
+        protected Dictionary<ulong, CollisionDataRB> m_collisionsLastFrame = new Dictionary<ulong, CollisionDataRB>();
+        protected Dictionary<ulong, CollisionDataRB> m_collisionsThisFrame = new Dictionary<ulong, CollisionDataRB>();
 
-        protected PhysicsMatrix f_physicsMatrix;
+        protected HashSet<ulong> m_triggersLastFrame = new HashSet<ulong>();
+        protected HashSet<ulong> m_triggersThisFrame = new HashSet<ulong>();
 
         #endregion
 
@@ -61,12 +68,23 @@ namespace LoonyEngine {
         }
 
         public virtual void Simulate() {
+            // Resetting print data
             f_checkStates.Clear();
             m_moved = 0;
             m_broadChecks = 0;
             m_narrowChecks = 0;
             m_triggers = 0;
             m_collisions = 0;
+
+            // Resetting the trigger and collider data
+            HashSet<ulong> dummyTriggers = m_triggersLastFrame;
+            m_triggersLastFrame = m_triggersThisFrame;
+            m_triggersThisFrame = dummyTriggers;
+            m_triggersThisFrame.Clear();
+            Dictionary<ulong, CollisionDataRB> dummyCollisions = m_collisionsLastFrame;
+            m_collisionsLastFrame = m_collisionsThisFrame;
+            m_collisionsThisFrame = dummyCollisions;
+            m_collisionsThisFrame.Clear();
         }
 
         #endregion
@@ -89,12 +107,10 @@ namespace LoonyEngine {
             f_checkStates[CalcRBID(rb1, rb2)] = CheckState.NarrowCheck;
             ++m_narrowChecks;
 
-            // TODO colelct this data
-            // TODO: I have to know if this collision is new or not
-
             // If none of them are triggers, that means that the collision shall be resolved
+            // Well, they cannot be resolved here already; the data has to be remembered until the end of the resolutions
             if (!rb1.ColliderData.IsTrigger && !rb2.ColliderData.IsTrigger) {
-                //TODO; check directly for collision + resolve if possible (resolve only if moving towards each other)
+                // TODO; Implement the other collisions
 
                 if (rb1.ColliderData.Collider2D is AABB) {
                     if (rb2.ColliderData.Collider2D is AABB) {
@@ -107,12 +123,22 @@ namespace LoonyEngine {
                         // Circle - AABB
                     } else {
                         // Circle - Circle
+                        CollisionDataRB cd = Intersections.CollisionCircleCircle((Circle)rb1.ColliderData.Collider2D, (Circle)rb2.ColliderData.Collider2D, rb1, rb2);
+                        if (cd.DidCollide) {
+                            m_collisionsThisFrame[CalcRBID(rb1, rb2)] = cd;
+                            m_collisionsLastFrame.Remove(CalcRBID(rb1, rb2));
+                            f_checkStates[CalcRBID(rb1, rb2)] = CheckState.Collision;
+                            ++m_collisions;
+                        }
+
                     }
                 }
 
                 // pass collision to the event
             } else { // this means not both are colliders, so I only fire a trigger event on both (if touching)
                 // In a trigger, we only care, whether they are touching or not
+                // TODO I can already call the trigger functions here
+
                 if (rb1.ColliderData.Collider2D is AABB) {
                     if (rb2.ColliderData.Collider2D is AABB) {
                         // this should not be possible in this example
@@ -120,6 +146,7 @@ namespace LoonyEngine {
                             // call triggers for 1 and send 2
                             // call triggers for 2 and send 1
                             f_checkStates[CalcRBID(rb1, rb2)] = CheckState.Trigger;
+                            m_triggersThisFrame.Add(CalcRBID(rb1, rb2));
                             ++m_triggers;
                         }
                     } else {
@@ -128,6 +155,7 @@ namespace LoonyEngine {
                             // call triggers for 1 and send 2
                             // call triggers for 2 and send 1
                             f_checkStates[CalcRBID(rb1, rb2)] = CheckState.Trigger;
+                            m_triggersThisFrame.Add(CalcRBID(rb1, rb2));
                             ++m_triggers;
                         }
 
@@ -139,6 +167,7 @@ namespace LoonyEngine {
                             // call triggers for 1 and send 2
                             // call triggers for 2 and send 1
                             f_checkStates[CalcRBID(rb1, rb2)] = CheckState.Trigger;
+                            m_triggersThisFrame.Add(CalcRBID(rb1, rb2));
                             ++m_triggers;
                         }
                     } else {
@@ -147,6 +176,7 @@ namespace LoonyEngine {
                             // call triggers for 1 and send 2
                             // call triggers for 2 and send 1
                             f_checkStates[CalcRBID(rb1, rb2)] = CheckState.Trigger;
+                            m_triggersThisFrame.Add(CalcRBID(rb1, rb2));
                             ++m_triggers;
                         }
                     }
